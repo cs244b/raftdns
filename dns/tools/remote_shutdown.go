2b278f103e1e290e18125ac59d41228890d5cdef
anchovYu <aureole@sjtu.edu.cn>
2020-05-16 16:57:16 -0700

    Feat(remote start): Support argument start from HTTP request body

--- dns/tools/remote_shutdown.go
+++ dns/tools/remote_shutdown.go
@@ -2,32 +2,35 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 var cmd *exec.Cmd
 
 func start_dnsserver(w http.ResponseWriter, req *http.Request) {
-	// TODO(xyyu): read in argv and pass to binary
-	// body, err := ioutil.ReadAll(req.Body)
-	// if err != nil {
-	// 	log.Printf("Cannot read /start body: %v\n", err)
-	// 	http.Error(w, "Bad start body", http.StatusBadRequest)
-	// 	return
-	// }
-	// argv := string(body)
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		log.Printf("Cannot read /start body: %v\n", err)
+		http.Error(w, "Bad start body", http.StatusBadRequest)
+		return
+	}
+	argvstr := string(body)
+	argv := strings.Split(argvstr, " ")
 
-	cmd = exec.Command("../dns_server", "--id", "1", "--cluster", "http://127.0.0.1:12379", "--port", "12380")
+	// cmd = exec.Command("../dns_server", "--id", "1", "--cluster", "http://127.0.0.1:12379", "--port", "12380")
+	cmd = exec.Command("../dns_server", argv...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	if err := cmd.Start(); err != nil {
 		log.Fatal(err)
 	}
-	log.Print("Starting running dns_server")
-	fmt.Fprintf(w, "Start running dns_server")
+	log.Print("Starting running dns_server with arguement %s", argvstr)
+	fmt.Fprintf(w, "Starting running dns_server with argument %s", argvstr)
 }
 
 func kill_dnsserver(w http.ResponseWriter, req *http.Request) {
